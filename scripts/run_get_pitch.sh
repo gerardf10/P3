5177dbc5e3544e46ffa73462491a1c5cb915f1ad
gerardf10 <gerard.ferret@estudiantat.upc.edu>
2025-05-02 09:27:08 +0200

    es pot executar

--- scripts/run_get_pitch.sh
+++ scripts/run_get_pitch.sh
@@ -1,4 +1,26 @@
-pitch.sh
+
+#pitch.sh
+#!/bin/bash
+
+# Establecemos que el código de retorno de un pipeline sea el del último programa con código de retorno
+# distinto de cero, o cero si todos devuelven cero.
+
+#set -o pipefail
+
+# Put here the program (maybe with path)
+
+#GETF0="get_pitch"
+
+#for fwav in pitch_db/train/*.wav; do
+ #   ff0=${fwav/.wav/.f0}
+  #  echo "$GETF0 $fwav $ff0 ----"
+  #  $GETF0 --clip --median --lowpass=350 $fwav $ff0 > /dev/null || { echo -e "\nError in $GETF0 $fwav $ff0" && exit 1; }
+#done
+
+#pitch_evaluate pitch_db/train/*.f0ref
+
+#exit 0
+
 #!/bin/bash
 
 # Establecemos que el código de retorno de un pipeline sea el del último programa con código de retorno
@@ -8,12 +30,14 @@ set -o pipefail
 # Put here the program (maybe with path)
 GETF0="get_pitch"
 
-for fwav in pitch_db/train/*.wav; do
+# Per a cada fitxer .wav en el directori
+for fwav in ~/P3/pitch_db/train/*.wav; do
     ff0=${fwav/.wav/.f0}
     echo "$GETF0 $fwav $ff0 ----"
     $GETF0 --clip --median --lowpass=350 $fwav $ff0 > /dev/null || { echo -e "\nError in $GETF0 $fwav $ff0" && exit 1; }
 done
 
-pitch_evaluate pitch_db/train/*.f0ref
+# Finalitzar l'operació amb l'evaluació
+pitch_evaluate ~/P3/pitch_db/train/*.f0ref
 
 exit 0
